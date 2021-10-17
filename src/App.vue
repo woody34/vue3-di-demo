8a73f0e733ffad6810695ae75eaab6c32da2c646
Matt Woodruff <woodruff1606@gmail.com>
2021-10-16 19:26:58 -0500

    init-commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref, provide, reactive } from 'vue'
+import { menuStateKey, menuStateFactory } from '@/providers/menuState';
+
+const state = menuStateFactory();
+provide(menuStateKey, state);
+
+</script>
+
+<template>
+  <div>
+    <header class="bg-white shadow" v-if="$route.meta.title">
+      <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <h1
+          @click="state.filters.showMobileMenu = !state.filters.showMobileMenu"
+          class="text-3xl font-bold leading-tight text-gray-900"
+        >
+          state.filters.showMobileMenu / {{ state.filters.showMobileMenu }}
+        </h1>
+      </div>
+    </header>
+    <main>
+      <router-view />
+    </main>
+  </div>
+</template>
---
+++ src/providers/menuState.ts
@@ -0,0 +1,145 @@
+import { InjectionKey, reactive } from "vue";
+
+export const currencies = ['CAD', 'USD', 'AUD', 'EUR', 'GBP'] as const;
+
+export const navigation = {
+  categories: [
+    {
+      name: 'Women',
+      featured: [
+        {
+          name: 'New Arrivals',
+          href: '#',
+          imageSrc: 'https://tailwindui.com/img/ecommerce-images/mega-menu-category-01.jpg',
+          imageAlt: 'Models sitting back to back, wearing Basic Tee in black and bone.',
+        },
+        {
+          name: 'Basic Tees',
+          href: '#',
+          imageSrc: 'https://tailwindui.com/img/ecommerce-images/mega-menu-category-02.jpg',
+          imageAlt: 'Close up of Basic Tee fall bundle with off-white, ochre, olive, and black tees.',
+        },
+        {
+          name: 'Accessories',
+          href: '#',
+          imageSrc: 'https://tailwindui.com/img/ecommerce-images/mega-menu-category-03.jpg',
+          imageAlt: 'Model wearing minimalist watch with black wristband and white watch face.',
+        },
+        {
+          name: 'Carry',
+          href: '#',
+          imageSrc: 'https://tailwindui.com/img/ecommerce-images/mega-menu-category-04.jpg',
+          imageAlt: 'Model opening tan leather long wallet with credit card pockets and cash pouch.',
+        },
+      ],
+    },
+    {
+      name: 'Men',
+      featured: [
+        {
+          name: 'New Arrivals',
+          href: '#',
+          imageSrc: 'https://tailwindui.com/img/ecommerce-images/mega-menu-01-men-category-01.jpg',
+          imageAlt: 'Hats and sweaters on wood shelves next to various colors of t-shirts on hangers.',
+        },
+        {
+          name: 'Basic Tees',
+          href: '#',
+          imageSrc: 'https://tailwindui.com/img/ecommerce-images/mega-menu-01-men-category-02.jpg',
+          imageAlt: 'Model wearing light heather gray t-shirt.',
+        },
+        {
+          name: 'Accessories',
+          href: '#',
+          imageSrc: 'https://tailwindui.com/img/ecommerce-images/mega-menu-01-men-category-03.jpg',
+          imageAlt:
+            'Grey 6-panel baseball hat with black brim, black mountain graphic on front, and light heather gray body.',
+        },
+        {
+          name: 'Carry',
+          href: '#',
+          imageSrc: 'https://tailwindui.com/img/ecommerce-images/mega-menu-01-men-category-04.jpg',
+          imageAlt: 'Model putting folded cash into slim card holder olive leather wallet with hand stitching.',
+        },
+      ],
+    },
+  ],
+  pages: [
+    { name: 'Company', href: '#' },
+    { name: 'Stores', href: '#' },
+  ],
+} as const
+
+export const categories = [
+  {
+    name: 'New Arrivals',
+    href: '#',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-01-category-01.jpg',
+  },
+  {
+    name: 'Productivity',
+    href: '#',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-01-category-02.jpg',
+  },
+  {
+    name: 'Workspace',
+    href: '#',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-01-category-04.jpg',
+  },
+  {
+    name: 'Accessories',
+    href: '#',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-01-category-05.jpg',
+  },
+  { name: 'Sale', href: '#', imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-01-category-03.jpg' },
+] as const;
+
+export const collections = [
+  {
+    name: 'Handcrafted Collection',
+    href: '#',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-01-collection-01.jpg',
+    imageAlt: 'Brown leather key ring with brass metal loops and rivets on wood table.',
+    description: 'Keep your phone, keys, and wallet together, so you can lose everything at once.',
+  },
+  {
+    name: 'Organized Desk Collection',
+    href: '#',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-01-collection-02.jpg',
+    imageAlt: 'Natural leather mouse pad on white desk next to porcelain mug and keyboard.',
+    description: 'The rest of the house will still be a mess, but your desk will look great.',
+  },
+  {
+    name: 'Focus Collection',
+    href: '#',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-01-collection-03.jpg',
+    imageAlt: 'Person placing task list card into walnut card holder next to felt carrying case on leather desk pad.',
+    description: 'Be more productive than enterprise project managers with a single piece of paper.',
+  },
+] as const
+
+export interface MenuState {
+  filters: {
+    showMobileMenu: boolean;
+  };
+  options: {
+    navigation: typeof navigation;
+    currencies: typeof currencies;
+    categories: typeof categories;
+    collections: typeof collections;
+  }
+}
+
+export const menuStateKey: InjectionKey<MenuState> = Symbol()
+
+export const menuStateFactory = (): MenuState => reactive({
+    filters: reactive({
+      showMobileMenu: false,
+    }),
+    options: reactive({
+      navigation,
+      currencies,
+      categories,
+      collections,
+    })
+  });
